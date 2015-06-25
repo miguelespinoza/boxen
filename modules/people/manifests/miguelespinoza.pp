1c1750b28752d133f7c8cf98c75e2e0e89a45aa8
Miguel Espinoza <miguele@miguels-mbp-2.acs.tech>
2015-06-25 15:08:54 -0400

    Excluded atom packages

--- modules/people/manifests/miguelespinoza.pp
+++ modules/people/manifests/miguelespinoza.pp
@@ -5,22 +5,22 @@ class people::miguelespinoza {
 	include sublime_text_2
 	include atom 
 
-	# install the linter package
-	atom::package { ['linter',
-					'atom-beautify',
-					'autocomplete-plus',
-      				'linter-puppet',
-      				'linter-puppet-lint',
-     				 'linter-puppet-parse',
-     				 'minimap']:
-     				 ensure => present, }
+# 	# install the linter package
+# 	atom::package { ['linter',
+# 					'atom-beautify',
+# 					'autocomplete-plus',
+#       				'linter-puppet',
+#       				'linter-puppet-lint',
+#      				 'linter-puppet-parse',
+#      				 'minimap']:
+#      				 ensure => present, }
 
-file { "/Users/${::luser}/.atom":
-    ensure => directory,
-  }
-file { "/Users/${::luser}/.atom/config.cson":
-    ensure => present,
-    source => 'puppet:///modules/people/miguelespinoza/atom/config.cson',
-  }
+# file { "/Users/${::luser}/.atom":
+#     ensure => directory,
+#   }
+# file { "/Users/${::luser}/.atom/config.cson":
+#     ensure => present,
+#     source => 'puppet:///modules/people/miguelespinoza/atom/config.cson',
+#   }
 
 }
