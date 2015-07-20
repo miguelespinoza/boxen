0292b86e7b2a8b756d535374b40592fb7f6914d9
Miguel Espinoza <miguelespinoza349@yahoo.com>
2015-07-20 10:17:02 -0400

    Added coffeescript

--- manifests/site.pp
+++ manifests/site.pp
@@ -86,7 +86,15 @@ node default {
 
   # install some npm modules
   nodejs::module { 'bower':
-    node_version => 'v0.10'
+    node_version => 'v0.12.7'
+  }
+
+  nodejs::module { 'bower':
+    node_version => 'v0.12.7'
+  }
+
+  nodejs::module { 'coffee-script':
+    node_version -> 'v0.12.7'
   }
 
 
