ef32d1b4ef5c216c7c5eee64433a388f0435a208
Miguel Espinoza <miguelespinoza349@yahoo.com>
2015-08-04 23:58:31 -0400

    Added atom packages

--- modules/people/manifests/miguelespinoza.pp
+++ modules/people/manifests/miguelespinoza.pp
@@ -1,11 +1,11 @@
 class people::miguelespinoza {
 	include iterm2::dev
 	include zsh
-	#include atom
+	include atom
 
   # Symlink to fix Atom install, see
   # https://github.com/caskroom/homebrew-cask/issues/4716
-  #file { '/usr/local': 
+  #file { '/usr/local':
    # ensure => link,
     #target => '/opt/boxen',
   #}
@@ -21,17 +21,17 @@ class people::miguelespinoza {
 # osx::recovery_message { 'If this Mac is found, please call 123-456-7890': }
 
 # atom packages
-#atom::package { 'linter': }
-#atom::package { 'atom-beautify':}
-#atom::package { 'go-plus':}
-#atom::package { 'zen':}
-#atom::package { 'webbox-color':}
-#atom::package { 'file-icons':}
-#atom::package { 'file-icon-supplement':}
+atom::package { 'linter': }
+atom::package { 'atom-beautify':}
+atom::package { 'go-plus':}
+atom::package { 'zen':}
+atom::package { 'webbox-color':}
+atom::package { 'file-icons':}
+atom::package { 'file-icon-supplement':}
 
 # atom material theme
-#atom::theme { 'atom-material-ui': }
-#atom::theme { 'atom-material-syntax': }
+atom::theme { 'atom-material-ui': }
+atom::theme { 'atom-material-syntax': }
 
 include dropbox
 include ohmyzsh
